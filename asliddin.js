dab0a848049d603e53246f1f6f89146040cf0bc1
Asliddin1 <asliddinabdujabborov55@gmail.com>
2021-08-06 19:48:35 +0500

    as

---
+++ asliddin.js
@@ -0,0 +1,39 @@
+// Asliddin_Abdujabborov
+// 3 - task
+
+alert(`Chet elga ketamiz.`);
+
+const uzs = prompt(`Alish qancha puling bor?`);
+
+const xarajat1 = 500 * 9433.34;
+const xarajat2 = 250 * 9433.34;
+const xarajat3 = 120 * 10354.03;
+
+const ketish_narxi = xarajat1 + xarajat2 + xarajat3;
+
+if (uzs >= ketish_narxi) {
+	console.log(`Oq yo'l, Alisher! Naqd ${ketish_narxi} so'mga tushyabsan lekin.`);
+} else {
+	console.log(`Alisher, ozgina sabr qilish kerak bo'lar ekan. Xarajat ${ketish_narxi} so'm bo'lib ketdi.`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
